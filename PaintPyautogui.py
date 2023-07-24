293eccaea9c0557e77e1d7d36f2694c1df0f06af
Ben-Rothman <benrothman12@gmail.com>
2023-07-23 23:02:23 -0400

    Commit 2

---
+++ PaintPyautogui.py
@@ -0,0 +1,86 @@
+import pyautogui
+import time
+
+pyautogui.moveTo(5, 1075, duration = 1)
+pyautogui.leftClick()
+time.sleep(0.2)
+pyautogui.typewrite("p")
+time.sleep(0.2)
+pyautogui.typewrite("a")
+time.sleep(0.2)
+pyautogui.typewrite("i")
+time.sleep(0.2)   
+pyautogui.typewrite("n")
+time.sleep(0.2)
+pyautogui.typewrite("t")
+time.sleep(0.2)
+pyautogui.press("enter")
+pyautogui.moveTo(1648, 120, duration = 1)
+pyautogui.click()
+pyautogui.moveTo(140, 36, duration = 1)
+pyautogui.click()
+pyautogui.moveTo(23, 78, duration = 1)
+pyautogui.click()
+
+# D
+pyautogui.moveTo(140, 240, duration = 1)
+pyautogui.dragTo(140, 600, duration = 0.001)
+pyautogui.moveTo(140, 240, duration = 0.001)
+pyautogui.dragTo(232, 323, duration = 0.001)
+pyautogui.dragTo(300, 430, duration = 0.001)
+pyautogui.dragTo(250, 525, duration = 0.001)
+pyautogui.dragTo(140, 600, duration = 0.001)
+
+# U
+pyautogui.moveTo(450, 240, duration = 0.001)
+pyautogui.dragTo(450, 600, duration = 0.001)
+pyautogui.dragTo(600, 600, duration = 0.001)
+pyautogui.dragTo(600, 240, duration = 0.001)
+
+# M
+pyautogui.moveTo(713, 600, duration = 0.001)
+pyautogui.dragTo(713, 242, duration = 0.001)
+pyautogui.dragTo(881, 468, duration = 0.001)
+pyautogui.dragTo(1048, 240, duration = 0.001)
+pyautogui.dragTo(1053, 600, duration = 0.001)
+
+# B
+pyautogui.moveTo(1200, 240, duration = 0.001)
+pyautogui.dragTo(1336, 291, duration = 0.001)
+pyautogui.dragTo(1348, 371, duration = 0.001)
+pyautogui.dragTo(1258, 423, duration = 0.001)
+pyautogui.dragTo(1204, 424, duration = 0.001)
+pyautogui.dragTo(1260, 450, duration = 0.001)
+pyautogui.dragTo(1369, 508, duration = 0.001)
+pyautogui.dragTo(1358, 582, duration = 0.001)
+pyautogui.dragTo(1206, 599, duration = 0.001)
+pyautogui.dragTo(1204, 424, duration = 0.001)
+pyautogui.dragTo(1200, 240, duration = 0.001)
+
+# A
+pyautogui.moveTo(768, 930, duration = 0.001)
+pyautogui.dragTo(900, 700, duration = 0.001)
+pyautogui.dragTo(1000, 930, duration = 0.001)
+pyautogui.moveTo(830, 820, duration = 0.001)
+pyautogui.dragTo(950, 820, duration = 0.001)
+
+# S
+pyautogui.moveTo(1220, 700, duration = 0.001)
+pyautogui.dragTo(1100, 740, duration = 0.001)
+pyautogui.dragTo(1223, 800, duration = 0.001)
+pyautogui.dragTo(1220, 900, duration = 0.001)
+pyautogui.dragTo(1120, 930, duration = 0.001)
+
+# S
+pyautogui.moveTo(1420, 700, duration = 0.001)
+pyautogui.dragTo(1300, 740, duration = 0.001)
+pyautogui.dragTo(1423, 800, duration = 0.001)
+pyautogui.dragTo(1420, 900, duration = 0.001)
+pyautogui.dragTo(1320, 930, duration = 0.001)
+
+pyautogui.moveTo(100, 650, duration = 0.001)
+pyautogui.dragTo(1400, 650, duration = 0.001)
+
+pyautogui.moveTo(750, 950, duration = 0.001)
+pyautogui.dragTo(1490, 950, duration = 0.001)
+
